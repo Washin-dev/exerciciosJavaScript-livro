dba34406b051f74b0215d2d3b0b4f546fdf60e30
washington <washingtonaraujocarvalho@gmail.com>
2026-04-25 00:40:09 -0300

    exercício 3, capítulo 1

---
+++ cap1/ex1-3/ex1_3.js
@@ -0,0 +1,10 @@
+// Terceiro programa em JavaScript: demonstra operações aritméticas com uma string numérica convertida implicitamente e explicitamente.
+const a = "20";
+const b = a * 2; // b = 40
+const c = a / 2; // c = 10
+const d = a - 2; // d = 18
+const e = Number(a) + 2; // e = 22
+alert("b: " + b + " - Multiplicação do número 20 por 2");
+alert("c: " + c + " - Divisão do número 20 por 2");
+alert("d: " + d + " - Subtração do número 20 por 2");
+alert("e: " + e + " - Adição do número 20 com 2");
