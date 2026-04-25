798664328d37972166a41d879019f583bd39073d
washington <washingtonaraujocarvalho@gmail.com>
2026-04-25 00:36:46 -0300

    exercício 1, capítulo 1

---
+++ cap1/ex1_1/ex1_1.js
@@ -0,0 +1,3 @@
+// Primeiro programa em JavaScript: exibe uma mensagem de boas-vindas em alerta e registra uma mensagem no console.
+alert("Bem-vindo ao mundo JavaScript!");
+console.log("Meu primeiro programa...");
