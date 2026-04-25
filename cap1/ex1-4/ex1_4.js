037f053e06ba72abdcdb6dc89dfe0ef96a03959c
washington <washingtonaraujocarvalho@gmail.com>
2026-04-25 00:42:54 -0300

    exercício 4, capítulo 1

---
+++ cap1/ex1-4/ex1_4.js
@@ -0,0 +1,9 @@
+// Quarto programa em JavaScript: demonstra o uso de typeof para verificar os tipos de diferentes variáveis.
+const fruta = "banana";
+const preco = 3.5;
+const levar = true;
+let novoValor;
+console.log(typeof fruta); // string
+console.log(typeof preco); // number
+console.log(typeof levar); // boolean
+console.log(typeof novoValor); // undefined
