781e333e1815fa2d2f7dcea7ed050ab132138890
washington <washingtonaraujocarvalho@gmail.com>
2026-04-25 00:44:08 -0300

    exercício 5, capítulo 1

---
+++ cap1/ex1-5/ex1_5.js
@@ -0,0 +1,4 @@
+// Quinto programa em JavaScript: solicita um número e exibe o dobro em um alerta.
+const num = prompt("Numero: ");
+const dobro = num * 2;
+alert("O dobro de " + num + " é: " + dobro);
